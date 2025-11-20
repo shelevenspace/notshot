0aa0b61869141252531cf67f3ca1a374554f835d
shelevenspace <157693283+shelevenspace@users.noreply.github.com>
2025-11-20 06:57:05 -0800

    change python on shortcuts

--- launch-notshot-instant.sh
+++ launch-notshot-instant.sh
@@ -3,6 +3,4 @@ if [ "$EUID" -eq 0 ]; then
     echo "Fatal error: DO NOT run this as root! Try again without elevation."
     exit 1
 fi
-cd ~/.notshot/notshot
-python3 notshot.py -a
-
+~/.notshot/bin/python3.12 ~/.notshot/notshot/notshot.py -a
